82b96bd85cbad592c27164458860f0cd1b1a4acc
mattbfishman <mbfishman@quinnipiac.edu>
2016-04-11 19:32:21 -0400

    Add meteor method for inserting into hacking db

--- client/newHackerForm/newHackerForm.js
+++ client/newHackerForm/newHackerForm.js
@@ -15,7 +15,7 @@ Template.newHackerForm.events({
       allegiance: allegiance
     }
 
-   Hackers.insert(newHacker);
+   Meteor.call('insertHackers', newHacker);
    alert(`Add: ${newHacker.legalName} (${newHacker.hackerName})`);
   }
 
